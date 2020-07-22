576dabe3ff5d3b9d8a7f1d5d5c8378c19436c9b1
Madeline <madannahunt@gmail.com>
2020-07-22 17:49:50 -0400

    rewriting proportions code to be more readable and removing redundant code

--- proportions/draft_proportions.py
+++ proportions/draft_proportions.py
@@ -1,18 +1,3 @@
-# ---------------------------------------------------------------------------
-# DSWE_V2_GDAL_Proportions_v1- Jake Shermeyer 4/26/2017
-# Calculates annual and semi-decadal proportions of water and mixed vegetation landcover
-# for a single path/row.  The path/row folder is specified in the Path_dir line below.
-# *** NOTE: This script only works with the new LSRP data with the new naming convention that was deployed in April 2017.
-
-
-# *****************DEPENDENCIES*******************************************
-# Python 2.7 64 bit, will not run on 32 bit Python
-# GDAL 2.1+
-# numpy 1.12
-#
-# *****************OUTPUT INFORMATION*****************************
-#
-# Import modules
 import numpy as np
 import gdal
 import os
@@ -20,12 +5,7 @@ import datetime
 import glob
 import sys
 
-# ***************************REQUIRED PATH/INPUT INFORMATION**************************
-# Specify the path for input data. This should be the output folder containing your DSWE output, each contained in their own scene specific subfolder.
-# Start year recommended to end with a 0 or 5 regardless of where your data starts.  This is for semi-decadal proportions that will span a 5 year stretch.
-# Ensure that your start year is less than the first year of your DSWE output.
 Path_dir = '/home/mad/DSWE_EaD/proportions/test_data/DSWE_out_backup'
-#os.path.join('~', 'DSWE_EaD', 'proportions', 'test_data', 'DSWE_out')
 Start_yr = 1985
 End_yr = 1995
 
@@ -131,97 +111,84 @@ for folder in paths:
         if NumRast>=1:
             count = 0
             for raster in Year_Interp:
-                interp = gdal.Open(os.path.abspath(raster))
-                print("Opened:", raster)
-                interpmaxextent_out= processing_dir + "/interp.tif"
-                #Expand every interpreted layer to the maximum extent of all data for the path/row
-                interp=gdal.Translate(interpmaxextent_out, interp, projWin = MaxExtent)
-                MaxGeo=interp.GetGeoTransform()
-                interp=interp.GetRasterBand(1).ReadAsArray()
-                shape=interp.shape
+                def open_interp(raster, processing_dir):
+                    interp = gdal.Open(os.path.abspath(raster))
+                    print("Opened:", raster)
+                    interpmaxextent_out= processing_dir + "/interp.tif"
+                    #Expand every interpreted layer to the maximum extent of all data for the path/row
+                    interp=gdal.Translate(interpmaxextent_out, interp, projWin = MaxExtent)
+                    MaxGeo=interp.GetGeoTransform()
+                    interp=interp.GetRasterBand(1).ReadAsArray()
+                    shape=interp.shape
+                    return interp, MaxGeo, shape
+                interp, MaxGeo, shape = open_interp(raster, processing_dir)
+
                 # Reclassify the interpreted layer
-                if count == 0:
+                def reclassify_interp_layer(interp):
+                    interp[np.where((interp_copy==255) | (interp_copy==9))] = 0
                     water=interp.copy()
-                    water[np.where((water == 255) | (water == 9))] = 0
                     water[np.where((water == 2))] = 1
                     water[np.where((water == 4) | (water == 3))] = 0
                     mixed=interp.copy()
-                    mixed[np.where((mixed == 255) | (mixed == 9))] = 0
                     mixed[np.where((mixed == 1) | (mixed == 2) | (mixed == 4))] = 0
                     mixed[np.where((mixed == 3))] = 1
                     nonwater=interp.copy()
                     nonwater[np.where((nonwater == 0))] = 4
-                    nonwater[np.where((nonwater == 255)| (nonwater == 9))] = 0
                     nonwater[np.where((nonwater == 1) | (nonwater == 2) | (nonwater == 3))] = 0
                     nonwater[np.where((nonwater == 4))] = 1
+                    return water, mixed, nonwater
+
+                if count == 0:
+                    water, mixed, nonwater = reclassify_interp_layer(interp)
                     count += 1
                     print(count, "of", NumRast, "scenes processed")
                 # Reclassify the interpreted layer and add it to previously reclassified interpreted layers
                 elif count > 0 and count < NumRast:
-                    water2=interp.copy()
-                    water2[np.where((water2 == 255)| (water2 == 9))] = 0
-                    water2[np.where((water2 == 2))] = 1
-                    water2[np.where((water2 == 4) | (water2 == 3))] = 0
-                    water= water + water2
-                    mixed2=interp.copy()
-                    mixed2[np.where((mixed2 == 255) | (mixed2 == 9))] = 0
-                    mixed2[np.where((mixed2 == 1) | (mixed2 == 2) | (mixed2 == 4))] = 0
-                    mixed2[np.where((mixed2 == 3))] = 1
-                    mixed=mixed + mixed2
-                    nonwater2=interp.copy()
-                    nonwater2[np.where((nonwater2 == 0))] = 4
-                    nonwater2[np.where((nonwater2 == 255) | (nonwater2 == 9))] = 0
-                    nonwater2[np.where((nonwater2 == 1) | (nonwater2 == 2) | (nonwater2 == 3))] = 0
-                    nonwater2[np.where((nonwater2 == 4))] = 1
-                    nonwater= nonwater + nonwater2
+                    water2, mixed2, nonwater2 = reclassify_interp_layer(interp)
+                    water += water2
+                    mixed += mixed2
+                    nonwater += nonwater2
                     count += 1
                     print(count, "of", NumRast, "scenes processed")
+            
+            def create_output_file(data, data_str, output_dir, PathRow, year, shape, MaxGeo, interpproj):
+                if 'processing' in output_dir:
+                    data_file = processing_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_" + data_str + "sum.tif"
+                if 'proportions' in output_dir:
+                    data_file = proportion_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_" + data_str + "_Proportion.tif"
+
+
+                data_out=driver.Create(data_file, shape[1], shape[0], 1, gdal.GDT_Byte)
+                data_out.SetGeoTransform(MaxGeo)
+                data_out.SetProjection(interpproj)
+                data_out.GetRasterBand(1).WriteArray(data)
+                return
+
             if count == NumRast:
                 year=str(Active_yr)
                 #Identify pixels that contain data
                 containsdata= water+mixed+nonwater
-                containsdata_output= processing_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_containsdatasum.tif"
-                containsdata_out=driver.Create(containsdata_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                containsdata_out.SetGeoTransform(MaxGeo)
-                containsdata_out.SetProjection(interpproj)
-                containsdata_out.GetRasterBand(1).WriteArray(containsdata)
+                create_output_file(containsdata, 'containsdata', processing_dir, PathRow, year, shape, MaxGeo, interpproj)
                 #Water pixels
-                water_output= processing_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_watersum.tif"
-                water_out=driver.Create(water_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                water_out.SetGeoTransform(MaxGeo)
-                water_out.SetProjection(interpproj)
-                water_out.GetRasterBand(1).WriteArray(water)
+                create_output_file(water, 'water', processing_dir ,PathRow, year, shape, MaxGeo, interpproj)
                 #Mixed pixels
-                mixed_output= processing_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_mixedsum.tif"
-                mixed_out=driver.Create(mixed_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                mixed_out.SetGeoTransform(MaxGeo)
-                mixed_out.SetProjection(interpproj)
-                mixed_out.GetRasterBand(1).WriteArray(mixed)
+                create_output_file(mixed, 'mixed', processing_dir, PathRow, year, shape, MaxGeo, interpproj)
                 #Non Water pixels
-                nonwater_output= processing_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_nonwatersum.tif"
-                nonwater_out=driver.Create(nonwater_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                nonwater_out.SetGeoTransform(MaxGeo)
-                nonwater_out.SetProjection(interpproj)
-                nonwater_out.GetRasterBand(1).WriteArray(nonwater)
+                create_output_file(nonwater, 'nonwater',processing_dir, PathRow, year, shape, MaxGeo, interpproj)
+                
                 #Calculate water proportion for the year
-                water_output= proportion_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_Water_Proportion.tif"
                 waterproportion= water/np.float32(containsdata)
                 waterproportion=waterproportion * 100
                 np.rint(waterproportion)
-                water_out=driver.Create(water_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                water_out.SetGeoTransform(MaxGeo)
-                water_out.SetProjection(interpproj)
-                water_out.GetRasterBand(1).WriteArray(waterproportion)
+                create_output_file(waterproportion, 'water', proportion_dir, PathRow, year, shape, MaxGeo, interpproj)
+
                 #Calculate mixed proportion for the year
                 mixed_output= proportion_dir + "/DSWE_V2_P1_" + PathRow + "_" + year + "_Mixed_Proportion.tif"
                 mixedproportion= mixed/np.float32(containsdata)
                 mixedproportion=mixedproportion * 100
                 np.rint(mixedproportion)
-                mixed_out=driver.Create(mixed_output, shape[1], shape[0], 1, gdal.GDT_Byte)
-                mixed_out.SetGeoTransform(MaxGeo)
-                mixed_out.SetProjection(interpproj)
-                mixed_out.GetRasterBand(1).WriteArray(mixedproportion)
-                print("Proportions completed for year", year        )
+                create_output_file(mixedproportion, 'mixed', proportion_dir, PathRow, year, shape, MaxGeo, interpproj)
+                print("Proportions completed for year", year)
         Active_yr+=1
 
     #Decadal Proportions calculations
