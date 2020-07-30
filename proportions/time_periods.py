0cbcf6e1dcd381ff54b39a0b1d105ceda5b4b777
Madeline <madannahunt@gmail.com>
2020-07-30 14:18:12 -0400

    proportions code done! need to test (issue #10)

--- proportions/time_periods.py
+++ proportions/time_periods.py
@@ -1,76 +1,80 @@
-import numpy as np
-import gdal
-import os
+'''
+Functions to sort files by time period and process data.
+'''
 import datetime
-import glob
-import sys
-from dateutil.rrule import rrule, MONTHLY 
-
+import numpy as np
+from dateutil.rrule import rrule, MONTHLY
 import utils
 
-def process_files(current_files, prop_dir, max_extent, current_time):
+def process_files(current_files, prop_dir, max_extent, time_str):
     '''
-    Process files in the current time period of interest
+    Process files in the current time period of interest.
+    INPUTS:
+        current_files : list of str : list of paths to current file
+        prop_dir : str : path to directory to store processed data
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+        time_str : str : time period for filename
+    RETURNS:
+       saves processed data in prop_dir
     '''
     if current_files == []:
+        # no file to be processed for this time period
         return
 
     for i, file in enumerate(current_files):
-        # open the file    
-        raster, MaxGeo, rasterproj = utils.open_raster(
-                file, prop_dir, max_extent)
-
-        #TODO figure out these two and if they need to be
-            # outputs from open_raster function, 
-            # and if they are the same every time
-        #print(MaxGeo)
-        #print(rasterproj)
-        #breakpoint()
-
+        # open the file
+        raster, geo_transform, projection = utils.open_raster(
+            file, prop_dir, max_extent)
 
         if i == 0:
-            shape = raster.shape #XXX or np.shape(raster) ?
+            shape = raster.shape
             # initialize arrays
-            openSW = np.zeros(shape)
-            partialSW = np.zeros(shape)
+            open_sw = np.zeros(shape)
+            partial_sw = np.zeros(shape)
             nonwater = np.zeros(shape)
+        else:
+            # ensure that geo_transform and projection stay the same
+            #XXX unsure if they would ever change
+            assert geo_transform_0 == geo_transform
+            assert projection_0 == projection
+        geo_transform_0 = geo_transform
+        projection_0 = projection
 
         # reclassify layer and add to previous
-        openSW_new, partialSW_new, nonwater_new = utils.reclassify(raster)
-        openSW += openSW_new
-        partialSW += partialSW_new
+        open_sw_new, partial_sw_new, nonwater_new = utils.reclassify(raster)
+        open_sw += open_sw_new
+        partial_sw += partial_sw_new
         nonwater += nonwater_new
 
     # maximum number any given pixel can be
     total_num = len(current_files)
-    
+
     # calculate proportions open and partial surface water
-    openSW_prop = utils.calculate_proportion(openSW, total_num)
-    partialSW_prop = utils.calculate_proportion(partialSW, total_num)
+    open_sw_prop = utils.calculate_proportion(open_sw, total_num)
+    partial_sw_prop = utils.calculate_proportion(partial_sw, total_num)
     nonwater_prop = utils.calculate_proportion(nonwater, total_num)
 
     # create output files
-    prop_data = [openSW_prop, partialSW_prop, nonwater_prop]
-    data_str = ['openSW', 'partialSW', 'nonwater']
+    prop_data = [open_sw_prop, partial_sw_prop, nonwater_prop]
+    data_str = ['open_sw', 'partial_sw', 'nonwater']
 
     for i, data in enumerate(prop_data):
         utils.create_output_file(data, data_str[i],
-                prop_dir, current_time,
-                MaxGeo, rasterproj)
-
-    return
+                prop_dir, time_str,
+                geo_transform, projection)
 
 
 def process_by_year(all_files, all_dates, prop_dir, max_extent):
-    ''' 
+    '''
     Process files yearly.
-    INPUTS: 
-        all_files : list of str : list of paths to all DSWE files of interest 
+    INPUTS:
+        all_files : list of str : list of paths to all DSWE files of interest
         all_dates : list of dates : list of dates of files of interest
         prop_dir : str : path to save output data
-        max_extent : list of float : max extent of all files; 
-            in form [minx, maxy, maxx, miny] 
-    RETURNS: 
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+    RETURNS:
         processed data saved in prop_dir
     '''
     all_years = [i.year for i in all_dates]
@@ -86,27 +90,27 @@ def process_by_year(all_files, all_dates, prop_dir, max_extent):
                 current_files.append(file)
 
         # process files in current year of interest
-        process_files(current_files, prop_dir, max_extent, current_time)
-        print(f'Proportions completed for {current_time}')
-    return
+        time_str = str(current_time)
+        process_files(current_files, prop_dir, max_extent, time_str)
+        print(f'Proportions completed for {time_str}')
 
 
 def process_by_month(all_files, all_dates, prop_dir, max_extent):
     '''
     Process files monthly.
-    INPUTS: 
-        all_files : list of str : list of paths to all DSWE files of interest 
+    INPUTS:
+        all_files : list of str : list of paths to all DSWE files of interest
         all_dates : list of dates : list of dates of files of interest
         prop_dir : str : path to save output data
-        max_extent : list of float : max extent of all files; 
-            in form [minx, maxy, maxx, miny] 
-    RETURNS: 
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+    RETURNS:
         processed data saved in prop_dir
     '''
     start_date = datetime.date(min(all_dates).year, 1, 1)
     end_date = datetime.date(max(all_dates).year+1, 1, 1)
 
-    for current_time in rrule(freq=MONTHLY, 
+    for current_time in rrule(freq=MONTHLY,
             dtstart=start_date, until=end_date):
         current_files = []
         # make a list of files in the current month of interest
@@ -117,24 +121,24 @@ def process_by_month(all_files, all_dates, prop_dir, max_extent):
                 current_files.append(file)
 
         # process files in current month of interest
-        process_files(current_files, prop_dir, max_extent, current_time)
-        print(f'Proportions completed for {current_time.strftime("%b %Y")}')
-    return
+        time_str = current_time.strftime("%b_%Y")
+        process_files(current_files, prop_dir, max_extent, time_str)
+        print(f'Proportions completed for {time_str}')
 
 
 def process_by_month_across_years(all_files, all_dates, prop_dir, max_extent):
     '''
-    Process files across all months for all years 
-    INPUTS: 
-        all_files : list of str : list of paths to all DSWE files of interest 
+    Process files across all months for all years
+    INPUTS:
+        all_files : list of str : list of paths to all DSWE files of interest
         all_dates : list of dates : list of dates of files of interest
         prop_dir : str : path to save output data
-        max_extent : list of float : max extent of all files; 
-            in form [minx, maxy, maxx, miny] 
-    RETURNS: 
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+    RETURNS:
         processed data saved in prop_dir
     '''
-    all_months = [i.month for i in all_dates] 
+    all_months = [i.month for i in all_dates]
     start_month = min(all_months)
     end_month = max(all_months)
 
@@ -145,24 +149,24 @@ def process_by_month_across_years(all_files, all_dates, prop_dir, max_extent):
             file_time = all_months[i]
             if file_time == current_time:
                 current_files.append(file)
-        
+
         # process files in current month of interest
-        process_files(current_files, prop_dir, max_extent, current_time)
-        month_str = datetime.date(1800,current_time,1)
-        print(f'Proportions completed for {month_str.strftime("%B")}')
-    return   
+        month_str = datetime.date(1800, current_time, 1)
+        time_str = month_str.strftime("%B")
+        process_files(current_files, prop_dir, max_extent, time_str)
+        print(f'Proportions completed for {time_str}')
 
 
 def process_by_semidecade(all_files, all_dates, prop_dir, max_extent):
     '''
     Process files semi-decadally (every 5 years)
-    INPUTS: 
-        all_files : list of str : list of paths to all DSWE files of interest 
+    INPUTS:
+        all_files : list of str : list of paths to all DSWE files of interest
         all_dates : list of dates : list of dates of files of interest
         prop_dir : str : path to save output data
-        max_extent : list of float : max extent of all files; 
-            in form [minx, maxy, maxx, miny] 
-    RETURNS: 
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+    RETURNS:
         processed data saved in prop_dir
     '''
     all_years = [i.year for i in all_dates]
@@ -179,9 +183,9 @@ def process_by_semidecade(all_files, all_dates, prop_dir, max_extent):
                 current_files.append(file)
 
         # process files in current range of interest
-        process_files(current_files, prop_dir, max_extent, current_time)
-        print(f'Proportions completed for {current_time} to {current_time+4}')
-    return
+        time_str = str(current_time) + '_' + str(current_time+4)
+        process_files(current_files, prop_dir, max_extent, time_str)
+        print(f'Proportions completed for {time_str}')
 
 
 def process_by_season(all_files, all_dates, prop_dir, max_extent):
@@ -193,13 +197,13 @@ def process_by_season(all_files, all_dates, prop_dir, max_extent):
         Spring        | Autumn        | 1 March
         Summer        | Winter        | 1 June
         Autumn        | Spring        | 1 Sept
-    INPUTS: 
-        all_files : list of str : list of paths to all DSWE files of interest 
+    INPUTS:
+        all_files : list of str : list of paths to all DSWE files of interest
         all_dates : list of dates : list of dates of files of interest
         prop_dir : str : path to save output data
-        max_extent : list of float : max extent of all files; 
-            in form [minx, maxy, maxx, miny] 
-    RETURNS: 
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
+    RETURNS:
         processed data saved in prop_dir
     '''
     start_date = datetime.date(min(all_dates).year-1, 12, 1)
@@ -217,9 +221,7 @@ def process_by_season(all_files, all_dates, prop_dir, max_extent):
                 current_files.append(file)
 
         # process files in current season of interest
-        process_files(current_files, prop_dir, max_extent, current_time)
         season = utils.find_season(current_time_range)
-        print(f'Proportions completed for {season} {current_time.year}')
-    return
-
-
+        time_str = season + '_' + str(current_time.year)
+        process_files(current_files, prop_dir, max_extent, time_str)
+        print(f'Proportions completed for {time_str}')
--- proportions/utils.py
+++ proportions/utils.py
@@ -1,29 +1,31 @@
 '''
 Utility functions for proportions.py
 '''
-import numpy as np
-import sys, getopt
-import gdal
-import os
 import datetime
-from dateutil.rrule import rrule, MONTHLY 
+import getopt
+import os
+import sys
+import gdal
+import numpy as np
+from dateutil.rrule import rrule, MONTHLY
 from dateutil.relativedelta import relativedelta
 
 def command_line_args(argv):
     '''
     Handle command line arguments.
-    INPUTS: 
+    INPUTS:
         argv : list of str : options and arguments from command line
     RETURNS:
         main_dir : str : main directory where data is located
-        DSWE_layer : str : DSWE layer to be used in calculations
+        dswe_layer : str : DSWE layer to be used in calculations
         time_period : str : time period to perform calculations over
     '''
     main_dir = ''
-    DSWE_layer = ''
+    dswe_layer = ''
     time_period = ''
     try:
-        options, remainder = getopt.getopt(argv, 'hd:l:t:', ['help','directory=', 'layer=', 'timeperiod='])
+        options, _ = getopt.getopt(argv, 'hd:l:t:',
+                ['help', 'directory=', 'layer=', 'timeperiod='])
     except getopt.GetoptError:
         help_message()
         sys.exit(2)
@@ -37,16 +39,16 @@ def command_line_args(argv):
         elif opt in ('-d', '--directory'):
             main_dir = arg
         elif opt in ('-l', '--layer'):
-            DSWE_layer = arg
+            dswe_layer = arg.upper()
         elif opt in ('-t', '--timeperiod'):
-            time_period = arg
+            time_period = arg.lower()
 
-    return main_dir, DSWE_layer, time_period
+    return main_dir, dswe_layer, time_period
 
 
 def help_message():
     '''
-    Help message to print for usage and options of 
+    Help message to print for usage and options of
     command line arguments.
     '''
     print('usage: proportions.py -d <directory> -l <layer> -t <timeperiod>')
@@ -56,12 +58,11 @@ def help_message():
     print('\t-t, --timeperiod\t<timeperiod>\ttime period to perform calculations over')
     print('\t\t\t\t{\'year\'|\'month\'|\'month_across_years\'|\'semidecade\'|\'season\'}')
     print('\t-h, --help\t\tprint help message')
-    return
 
 
 def get_file_date(filename):
     '''
-    Gets date that file data was collected; 
+    Gets date that file data was collected;
     uses convention of scene_id (entity_id) as filename.
     '''
     file_year = int(filename[15:19])
@@ -71,13 +72,13 @@ def get_file_date(filename):
     return file_date
 
 
-def get_files(main_dir, DSWE_layer):
+def get_files(main_dir, dswe_layer):
     '''
     Creates list of DSWE files with the layer of interest
     and list of file dates.
-    INPUTS: 
+    INPUTS:
         main_dir : str : main directory where data is located
-        DSWE_layer : str : DSWE layer to be used in calculations
+        dswe_layer : str : DSWE layer to be used in calculations
     RETURNS:
         all_files : list of str : list of all relevant DSWE file paths
         all_dates : list of dates : list of all file dates
@@ -85,11 +86,11 @@ def get_files(main_dir, DSWE_layer):
     all_files = []
     all_dates = []
     # look through all directories and subdirectories
-    for dirpath, dirnames, filenames in os.walk(main_dir):
+    for dirpath, _, filenames in os.walk(main_dir):
         # find all files in tree
         for filename in filenames:
             # if the file is the layer of interest
-            if DSWE_layer in filename:
+            if dswe_layer in filename:
                 all_files.append(os.path.join(dirpath, filename))
                 file_date = get_file_date(filename)
                 all_dates.append(file_date)
@@ -102,7 +103,7 @@ def make_output_dir(main_dir, time_period):
     INPUTS:
         main_dir : str : main directory where data is located
     RETURNS:
-        prop_dir : str : path to directory where results will be stored 
+        prop_dir : str : path to directory where results will be stored
     '''
     prop_dir = os.path.join(main_dir, f'proportions_{time_period}')
     os.makedirs(prop_dir, exist_ok=True)
@@ -112,14 +113,14 @@ def make_output_dir(main_dir, time_period):
 def find_max_extent(file, extent_0):
     '''
     Determine the maximum extent of all files;
-    ensures that no images are cropped and that 
+    ensures that no images are cropped and that
     extent/georeferencing remains consistent
     INPUTS:
         file : str : path to current file being processed
-        extent_0 : list : initial extent values to be overwritten; 
+        extent_0 : list : initial extent values to be overwritten;
             in order [minx, maxy, maxx, miny]
     RETURNS:
-        extent_0 : list : updated extent values 
+        extent_0 : list : updated extent values
     '''
     # calculate extent of current file
     raster = gdal.Open(os.path.abspath(file))
@@ -129,22 +130,23 @@ def find_max_extent(file, extent_0):
     maxy = geo_transform[3]
     maxx = minx + geo_transform[1] * raster.RasterXSize
     miny = maxy + geo_transform[5] * raster.RasterYSize
-    
+
     # rewrite extent with larger values
-    #TODO check if this is correct -- appears to be decreasing every time (finding minimum instead of maximum extent)
+    #XXX is this correct? appears to be decreasing every time
+        # (finding minimum instead of maximum extent)
     if minx < extent_0[0]:
         extent_0[0] = minx
     if maxy > extent_0[1]:
-        extent_0[1]=maxy
+        extent_0[1] = maxy
     if maxx > extent_0[2]:
-        extent_0[2]=maxx
+        extent_0[2] = maxx
     if miny < extent_0[3]:
-        extent_0[3]=miny
+        extent_0[3] = miny
     return extent_0
 
 
 def find_season_time_range(current_time):
-    '''   
+    '''
     Find time range of a particular season.
     INPUTS:
         current_time : date : current time (beginning of season)
@@ -152,7 +154,7 @@ def find_season_time_range(current_time):
         current_time_range : list of int : list of months in season
     '''
     until_time = current_time + relativedelta(months=2)
-    
+
     current_time_range = list(rrule(freq=MONTHLY,
             dtstart=current_time, until=until_time))
     current_time_range = [i.month for i in current_time_range]
@@ -164,7 +166,7 @@ def find_season(current_time_range):
     Assign a string to the season in the N. Hemisphere.
     INPUTS:
         current_time_range : list of int : list of months in the season
-    RETURNS:    
+    RETURNS:
         season : str : season in N. Hemisphere
     '''
     if 12 in current_time_range:
@@ -182,129 +184,100 @@ def open_raster(file, prop_dir, max_extent):
     '''
     Open file and read as array.
     INPUTS:
-        file : 
-        process_dir : 
-        max_extent : 
+        file : str : path to current file to open
+        prop_dir : str : path to directory to temporarially save raster
+        max_extent : list of float : max extent of all files;
+            in form [minx, maxy, maxx, miny]
     RETURNS:
-        raster : 
-        MaxGeo : 
-        shape : 
+        raster : numpy array : array form of raster image
+        geo_transform : tuple : raster positions related to
+            georeferences coordinates by affine transform
+        projection : list : GDAL projection metadata
     '''
-    #TODO document this function
-
+    # open the current file
     raster = gdal.Open(os.path.abspath(file))
-    #print("Opened:", file)
-    rastermaxextent_out= prop_dir + "/raster.tif"
-    #TODO is the above needed anymore
-    #Expand every rasterreted layer to the maximum extent of all data for the path/row
-    raster=gdal.Translate(rastermaxextent_out, raster, projWin = max_extent)
-    MaxGeo=raster.GetGeoTransform()
-    rasterproj = raster.GetProjection()
-    raster=raster.GetRasterBand(1).ReadAsArray()
-    return raster, MaxGeo, rasterproj
+
+    # expand layer to max extent of all data for the path/row
+    raster_max_extent = os.path.join(prop_dir, 'raster.tif')
+    raster = gdal.Translate(raster_max_extent, raster, projWin=max_extent)
+
+    geo_transform = raster.GetGeoTransform()
+    projection = raster.GetProjection()
+    raster = raster.GetRasterBand(1).ReadAsArray()
+    return raster, geo_transform, projection
 
 
 def reclassify(raster):
     '''
-    Reclassify the layer with observations of interest 
-    as 1 and observations not of interest as 0; 
+    Reclassify the layer with observations of interest
+    as 1 and observations not of interest as 0;
     invalid observations are mapped to -1e12
     INPUTS:
-        raster : 
+        raster : numpy array : array of image raster
     RETURNS:
-        openSW : 
-        partialSW : 
-        nonwater : 
+        open_sw : numpy array : open surface water pixels
+        partial_sw : numpy array : partial surface water pixels
+        nonwater : numpy array : non-water pixels
     DSWE CLASSIFICATION: for INTR and INWM layers
         Pixel Value | rasterretation
-            0       |   not openSW
-            1       |   openSW, high confidence
-            2       |   openSW, mod confidence
+            0       |   not open_sw
+            1       |   open_sw, high confidence
+            2       |   open_sw, mod confidence
             3       |   potential wetland
-            4       |   openSW/wetland, low confidence
+            4       |   open_sw/wetland, low confidence
             9       |   cloud/snow (INWM only)
             255     |   no data
     REFERENCES:
         LANDSAT DSWE Product Guide, pg. 10
-
     '''
     # INVALID observations: no data or cloud/snow
     invalid = np.zeros(np.shape(raster))
-    invalid[(raster==9) | (raster==255)] = float('nan')
+    invalid[(raster == 9)|(raster == 255)] = float('nan')
 
     # NO INUNDATION: nonwater
-    nonwater = (raster==0).astype(float) + invalid
-    
+    nonwater = (raster == 0).astype(float) + invalid
+
     # OPEN SURFACE WATER: water, high/mod confidence
-    openSW = ((raster==1) | (raster==2)).astype(float) + invalid
+    open_sw = ((raster == 1)|(raster == 2)).astype(float) + invalid
 
     # PARTIAL SURFACE WATER: wetland, water low confidence
-    partialSW = ((raster==3) | (raster==4)).astype(float) + invalid
-    
-    return openSW, partialSW, nonwater
+    partial_sw = ((raster == 3)|(raster == 4)).astype(float) + invalid
+    return open_sw, partial_sw, nonwater
 
 
 def calculate_proportion(data, total_num):
     '''
     Calculate proportion of time each pixel in data is spent
-    in that state, out of 1.0
+    in that state, out of 1.0.
     '''
     proportion = data / total_num
-
     return proportion
 
 
-def create_output_file(data, data_str, output_dir, current_time, MaxGeo, rasterproj):
+def create_output_file(data, data_str, prop_dir, time_str, geo_transform, projection):
     '''
     Creates output file for data in designated directory
     INPUTS:
-        data : 
-        data_str : str : 
-        output_dir : str : 
-        year : 
-        shape : 
-        MaxGeo : 
-        rasterproj : 
+        data : numpy array : processed raster data
+        data_str : str : string corresponding with type of data
+        prop_dir : str : path to output directory
+        time_str : str : time period for filename
+        geo_transform : tuple : raster positions related to
+            georeferences coordinates by affine transform
+        projection : list : GDAL projection metadata
     RETURNS:
         output file in output_dir
     '''
-    
     # create output filename
-    #TODO neaten these up
-    filename = 'DSWE_V2_P1_' + str(current_time) + '_' + data_str + '_Proportion.tif'
-    # output file path
-    file_path = os.path.join(output_dir, filename)
+    filename = time_str + data_str + '_proportion.tif'
+    file_path = os.path.join(prop_dir, filename)
 
     shape = data.shape
-    # create output file
 
+    # create output file
     driver = gdal.GetDriverByName('GTiff') # save as geotiff
     outdata = driver.Create(file_path, shape[1], shape[0], 1, gdal.GDT_Byte)
-    outdata.SetGeoTransform(MaxGeo)
-    outdata.SetProjection(rasterproj)
+    outdata.SetGeoTransform(geo_transform)
+    outdata.SetProjection(projection)
     outdata.GetRasterBand(1).WriteArray(data)
     outdata.FlushCache()
-
-    return
-
-
-def years_to_process(files):
-    year_files = []
-    for file in files:
-        file_year = int(file[18:22])
-        print('CHECK year should be', file_year)
-        if file_year >= Active_yr and file_year <= Range_yr:
-            year_files.append(file)
-    return year_files
-
-def sum_annual_files(year_files, shape):
-    data = np.zeros(shape)
-    for file in year_files:
-        new_data = gdal.Open(os.path.abspath(file))
-        new_data = new_data.GetfileBand(1).ReadAsArray()
-        data += new_data
-    return data
-
-
-
-
